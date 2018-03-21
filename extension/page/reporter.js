c02d8f46a95e29ed327e6adf65d41ef23c70a16c
Blake Winton <bwinton@latte.ca>
2018-03-20 20:57:03 -0400

    Re-style the popup.
    * Use browser-styles.
    * Move the attribution to the options.
    * Just close the popup when we report it.
    * Move things around a lot.
    * Other stuff I forgot.
    
    Fixes #9, #7, #5, #4, #3.

--- extension/page/reporter.js
+++ extension/page/reporter.js
@@ -24,6 +24,7 @@ function queryTabs(options) {
 
 onload = function() {
     var urlField = document.getElementById("url");
+    var favicon = document.getElementById("favicon");
     let options = {active: true};
     getPlatformInfo()
       .then(info => info.os)
@@ -35,42 +36,24 @@ onload = function() {
       })
       .then(tabs => {
           for (let tab of tabs) {
-              return tab.url;
+              return tab;
           }
       })
-      .then(url => urlField.value = url);
+      .then(tab => {
+          urlField.value = tab.url;
+          favicon.src = tab.favIconUrl;
+      });
 
-    var submit = document.querySelector("input[type=submit]");
+    var submit = document.getElementById("report");
     submit.onclick = function() {
         report();
         return false;
     };
 };
 
-chrome.runtime.onMessage.addListener(notify);
-
-function notify(message) {
-    let status = document.getElementById("status");
-    switch (message.type) {
-    case "success":
-        status.innerText = 'Done, thanks for your report!';
-        setTimeout(function() {
-            status.innerText = '';
-        }, 1000);
-        break;
-    case "error":
-        status.innerText = 'Error occurred: ' + message.err;
-        setTimeout(function() {
-            status.innerText = '';
-        }, 5000);
-        break;
-    }
-}
-
 function report() {
     let url = document.getElementById("url");
     let desc = document.querySelector("textarea");
-    let status = document.getElementById("status");
-    status.innerText = "Sending...";
     chrome.runtime.sendMessage({url: url.value, desc: desc.value, type:"fetch"});
+    window.close();
 }
